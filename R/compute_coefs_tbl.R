15662987c549ffd928d3d8faabe48c203365de77
Philippe Boileau <philippe.boileau@roche.com>
2021-09-27 09:50:29 -0400

    tidying up compute_coefs_tbl()

--- R/compute_coefs_tbl.R
+++ R/compute_coefs_tbl.R
@@ -1,7 +1,8 @@
-#' Compute Linear Model Coefficients and Standard Errors
+#' Compute Cross-Validated Estimate and its Standard Errors
 #'
-#' \code{compute_coefs_tbl()} estimates the univariate linear model coefficients
-#' of each biomarker, and computes their accompanying standard errors.
+#' \code{compute_coefs_tbl()} estimates the linear model coefficients of each
+#'   biomarker's projected conditional average treatment effect, and computes
+#'   their associated standard errors.
 #'
 #' @param cv_ls A \code{list} containing two \code{tibbles}. The first,
 #'   \code{betas_df}, contains the estimated beta coefficients for each
@@ -20,7 +21,7 @@ compute_coefs_tbl <- function(cv_ls) {
   betas <- colMeans(cv_ls$betas_df)
 
   # compute the standard errors
-  ses <- sqrt(colMeans((cv_ls$ic_df^2)) / nrow(cv_ls$ic_df))
+  ses <- sqrt(colMeans(cv_ls$ic_df^2) / nrow(cv_ls$ic_df))
 
   # create the tibble
   betas_tbl <- tibble::tibble(
