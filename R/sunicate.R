7815c984570e6d70c07d0b4a6776e805140440a7
Philippe Boileau <philippe.boileau@roche.com>
2022-01-04 09:43:35 -0500

    adding mock sunicate function

--- R/sunicate.R
+++ R/sunicate.R
@@ -1,6 +1,6 @@
-#' Univariate Conditional Average Treatment Effect Estimation
+#' Univariate Conditional Average Treatment Effect Estimation For Right-Censored Time-To-Event Outcomes
 #'
-#' \code{unicate()} estimates a linear approximation of the conditional
+#' \code{sunicate()} estimates a linear approximation of the conditional
 #'   average treatment effect for each variable specified in the
 #'   \code{biomarkers} argument. The estimated coefficients are then used to
 #'   test whether their corresponding biomarkers are treatment-effect modifiers.
@@ -8,12 +8,17 @@
 #'   cross-validated empirical influence curves to estimate these coefficients'
 #'   standard errors.
 #'
-#' @param data A \code{data.frame} or \code{tibble} object containing the
-#'   outcome variable, treatment indicator, and covariates. Note that the
-#'   biomarkers must be a subset of the covariates.
-#' @param outcome A \code{character} defining the name of the outcome variable
-#'   in \code{data}. The outcome must be a continuous or a binary factor
-#'   variable.
+#' @param data A wide \code{data.frame} or \code{tibble} object containing the
+#'   status (event variable), relative time of the event, treatment indicator,
+#'   and covariates. Note that the biomarkers must be a subset of the
+#'   covariates, and that there should only be one row per observation.
+#' @param status A \code{character} defining the name of the status variable
+#'   in \code{data}. This binary variable indicates whether the observation
+#'   failed at the associated \code{relative_time}, or if it was censored.
+#'   Failures should be represented by a \code{1}, and censoring events by a
+#'   \code{0}.
+#' @param relative_time A \code{character} providing the name of the time
+#'   variable in \code{data}.
 #' @param treatment A \code{character} indicating the name of the binary
 #'   treatment variable in \code{data}.
 #' @param covariates A \code{character} vector listing the covariates in
@@ -21,11 +26,17 @@
 #' @param biomarkers A \code{character} vector listing the biomarkers of
 #'   interest in \code{data}. \code{biomarkers} must be a subset of
 #'   \code{covariates}.
-#' @param super_learner A \code{\link[sl3:Lrnr_sl]{SuperLearner}} object. If set
-#'   to \code{NULL}, the default SuperLearner is used. The default's library
-#'   consists of a linear model, penalized linear models (LASSO and elasticnet),
-#'   a spline regression, a General Additive Model, XGBoost, a Random Forest,
-#'   and the mean model.
+#' @param falure_super_learner A \code{\link[sl3:Lrnr_sl]{SuperLearner}} object
+#'   used to estimate the conditional failure model. If set to \code{NULL}, the
+#'   default SuperLearner is used. The default's library consists of a linear
+#'   model, penalized linear models (LASSO and elasticnet), a spline regression,
+#'   a General Additive Model, XGBoost, a Random Forest, and the mean model.
+#' @param censore_super_learner A \code{\link[sl3:Lrnr_sl]{SuperLearner}} object
+#'   used to estimate the conditional censoring model. If set to \code{NULL},
+#'   the default SuperLearner is used. The default's library consists of a
+#'   linear model, penalized linear models (LASSO and elasticnet), a spline
+#'   regression, a General Additive Model, XGBoost, a Random Forest, and the
+#'   mean model.
 #' @param propensity_score_ls A named \code{numeric} \code{list} providing the
 #'   propensity scores for the treatment levels. The first element of the list
 #'   should correspond to the "treatment" group, and the second to the "control"
@@ -43,29 +54,33 @@
 #'   significance.
 #'
 #' @export
-unicate <- function(
+sunicate <- function(
   data,
-  outcome,
+  status,
+  relative_time,
   treatment,
   covariates,
   biomarkers,
-  super_learner = NULL,
+  failure_super_learner = NULL,
+  censoring_super_learner = NULL,
   propensity_score_ls,
   v_folds = 5L,
   parallel = FALSE
 ) {
 
   # assess the data quality and formatting, and prepare it for analysis
-  data <- prep_data(data, outcome, treatment, covariates, biomarkers)
+  data <- prep_longitudinal_data(
+    data, status, relative_time, treatment, covariates, biomarkers
+  )
 
   # compute CV coefficients and CV influence curves
-  cv_ls <- estimate_univariate_cates(
-    data, outcome, treatment, biomarkers, super_learner, propensity_score_ls,
-    v_folds, parallel
+  cv_ls <- estimate_univariate_survival_cates(
+    data, status, relative_time, treatment, biomarkers, failure_super_learner,
+    censoring_super_learner, propensity_score_ls, v_folds, parallel
   )
 
   # compute the table of biomarker coefficients and standard errors
-  biomarkers_tbl <- compute_coefs_tbl(cv_ls)
+  biomarkers_tbl <- compute_survival_coefs_tbl(cv_ls)
 
   # perform tests using the estimated coefficients and standard errors
   biomarkers_tbl <- perform_inference(biomarkers_tbl)
